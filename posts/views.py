3e0f85b49ca23b6640ccc330e624fa7f2aedf51a
yongun2 <djkye0001@naver.com>
2023-05-18 02:03:46 +0900

    [fix] calculator 위치 수정

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,34 @@
 from rest_framework.viewsets import ModelViewSet
 from posts.models import Post
-from posts.serializers import PostModelSerializer
+from posts.serializers import PostListModelSerializer, PostModelSerializer
 
 
 class PostModelViewSet(ModelViewSet):
     queryset = Post.objects.all()
-    serializer_class = PostModelSerializer
+    serializer_class = PostListModelSerializer
+
+def calculate(request):
+    # 1. 데이터 확인
+    num1 = request.GET.get("num1", 0)
+    num2 = request.GET.get("num2", 0)
+    operators = request.GET.get("operators")
+    
+    # 2. 계산
+    if operators == "+":
+        result = int(num1) + int(num2)
+    elif operators == "-":
+        result = int(num1) - int(num2)
+    elif operators == "*":
+        result = int(num1) * int(num2)
+    elif operators == "/":
+        result = int(num1) / int(num2)
+    else:
+        result = 0
+    
+    data  = {
+        "type": "FBV",
+        "result": result
+    }
+
+    # 3. 응답
+    return Response(data=data, status=status.HTTP_200_OK)
